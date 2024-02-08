9e199e261894a403e98797fd1aa09fe694496b22
lognathparamasivam <loganathan.paramasivam@zemosolabs.com>
2024-02-08 14:07:25 +0530

    Initial Commit

---
+++ src/main/java/com/hyp/HypBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.hyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypBackendApiApplication.class, args);
+	}
+
+}
