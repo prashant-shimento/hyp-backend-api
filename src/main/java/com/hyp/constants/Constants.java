46bac5d0361623c5852cd21803c2ca7b8361dd33
Aasif Ali <108855101+AliAasif123@users.noreply.github.com>
2024-10-11 10:03:47 +0530

    search by fields name in restaurant has been fixed (#77)

--- src/main/java/com/hyp/constants/Constants.java
+++ src/main/java/com/hyp/constants/Constants.java
@@ -30,7 +30,8 @@ public interface Constants {
 			"restaurantId");
 	List<String> DATE_API_PARAMS = Arrays.asList("createdAt", "orderTime");
 	List<String> CATEGORIES_API_PARAMS = Arrays.asList("id", "restaurantId", "categoryName", "active");
-	List<String> PARTNER_API_PARAMS = Arrays.asList("id","name", "domain", "active", "type");
+	List<String> PARTNER_API_PARAMS = Arrays.asList("id", "name", "domain", "active", "type");
+	List<String> RESTAURANT_API_PARAMS = Arrays.asList("restaurantName","menuSharingCode","contact");
 
 	// Constants for logging API integration
 	String INBOUND_API_LOG = "InboundAPILog";
