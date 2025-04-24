a3f999b9ff64fdd649420e2391f8fb55895b8271
Aasif Ali <108855101+AliAasif123@users.noreply.github.com>
2025-04-24 16:59:16 +0530

    tax issue fixed (#130)

--- src/main/java/com/hyp/dto/TaxDto.java
+++ src/main/java/com/hyp/dto/TaxDto.java
@@ -4,14 +4,14 @@ import lombok.Data;
 
 @Data
 public class TaxDto extends BaseDto {
-	private String taxname;
+	private String taxName;
 	private String tax;
-	private String taxtype;
-	private String tax_ordertype;
+	private String taxType;
+	private String taxOrderType;
 	private String active;
-	private String tax_coreortotal;
-	private String tax_taxtype;
+	private String taxCoreOrTotal;
+	private String taxTaxType;
 	private String rank;
-	private String consider_in_core_amount;
+	private String considerInCoreAmount;
 	private String description;
 }
